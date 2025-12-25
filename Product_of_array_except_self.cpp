23f586279e4bc93feb44486aaeec58a446b3571a
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2025-12-25 20:34:43 +0530

    Product of array except self

---
+++ Product_of_array_except_self.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> result(n);
+        int prod = 1;
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = prod;
+            prod *= nums[i];
+        }
+        prod = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            result[i] *= prod;
+            prod *= nums[i];
+        }
+        return result;
+    }
+};
