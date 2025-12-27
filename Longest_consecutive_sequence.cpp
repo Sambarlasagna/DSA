586eadda4c10261a0a2479a77948e135062b2517
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2025-12-27 20:45:43 +0530

    moved empty list check before sort

--- Longest_consecutive_sequence.cpp
+++ Longest_consecutive_sequence.cpp
@@ -4,30 +4,30 @@ class Solution
 public:
     int longestConsecutive(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-        int maxx = 0;
-        int count = 0;
         if (nums.size() == 0)
         {
             return 0;
         }
+        sort(nums.begin(), nums.end());
+        int maxx = 0;
+        int count = 1;
         for (int i = 1; i < nums.size(); i++)
         {
             if (nums[i] - nums[i - 1] == 1)
             {
-                cout << count << " ";
                 count++;
             }
             else if (nums[i] - nums[i - 1] == 0)
             {
+                continue;
             }
             else
             {
-                maxx = max(count + 1, maxx);
-                count = 0;
+                maxx = max(count, maxx);
+                count = 1;
             }
         }
-        maxx = max(count + 1, maxx);
+        maxx = max(count, maxx);
         return maxx;
     }
 };
