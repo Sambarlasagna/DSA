043b6129592e77452d92c7adb717c1fcd6e96d4b
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2026-01-19 23:12:05 +0530

    find first palindromic string in the array

---
+++ find_first_palindromic_string_in_the_array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool check(string s){
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+        for(string s:words){
+            if(check(s)){
+                return s;
+            }
+        }
+        return "";
+    }
+};
